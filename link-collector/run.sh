aff1a336ee079b933ca83e9da6351beef68423de
Chiu-Ki Chan <chiuki@gmail.com>
2015-12-27 19:39:03 +0100

    Update dates and titles extraction for OS X Yosemite

--- link-collector/run.sh
+++ link-collector/run.sh
@@ -16,7 +16,7 @@ for url in `grep message-link ${INDEX_FILE} | cut -f4 -d\"`; do
   month=`echo ${basename} | cut -f1 -d\-`
   day=`echo ${basename} | cut -f2 -d\- | sed -e 's/[a-z]*$//g'`
   year=`echo ${basename} | cut -f3 -d\- | sed -e 's/20115/2015/g'`
-  published_date=`date -d "${month} ${day} ${year}" +"%Y%m%d"`
+  published_date=`date -v ${year}y -v${month} -v ${day}d +"%Y%m%d"`
   output=${RAW_DIR}/${published_date}.html
   if [ ! -f ${output} ]; then
     curl ${url} > ${output}
@@ -26,15 +26,17 @@ done
 for file in ${RAW_DIR}/2???????.html; do
   echo ${file}
   published_date=`basename ${file} | cut -f1 -d.`
-  formatted_date=`date -d ${published_date} +'%-d-%b-%Y'`
+  day=`echo ${published_date:6:2}`
+  month=`echo ${published_date:4:2} | sed -e 's/^0//'`
+  formatted_date=`date -v ${published_date:0:4}y -v${month}m -v ${day}d +'%-d-%b-%Y'`
   grep -q ${formatted_date} ${OUTPUT_FILE}
   if [ $? -eq 0 ]; then
     echo "  Already processed"
   else
     count=0
     for url in `egrep -o "\[.*a.href.*\[.*tweet it.*\]" ${file} | cut -f2 -d\"`; do
-      url_effective=`curl -Ls -o ${TMP_FILE} -w %{url_effective} ${url}`
-      title=`awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");print;exit}' ${TMP_FILE}`
+      result=`curl -Ls -o ${TMP_FILE} -w %{url_effective} ${url}`
+      title=`cat ${TMP_FILE} | tr -d "\n\r" | grep -ioEm1 "<title[^>]*>[^<]*</title" | cut -f2 -d\> | cut -f1 -d\<`
       rm -f ${TMP_FILE}
       echo -n ${formatted_date} >> ${OUTPUT_FILE}
       echo -en '\t' >> ${OUTPUT_FILE}
